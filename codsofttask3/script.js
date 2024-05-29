3dd315429baae24ada2547fc842c5ba67681f493
Mounika suri <147294897+mounikasuri@users.noreply.github.com>
2024-05-29 14:20:23 +0530

    calculator

---
+++ codsofttask3/script.js
@@ -0,0 +1,18 @@
+const dis=document.getElementById("dis");
+
+function appendToDisplay(input){
+    dis.value +=input;
+}
+
+function clearDisplay(){
+    dis.value=" ";
+}
+
+function calculate(){
+    try{
+        dis.value=eval(dis.value);
+    }
+    catch(error){
+        dis.value="Error";
+    }
+}
